950067fd26934a87594335aad42eb34f249e2bd5
Anatoly Stansler <anatoly@sourcerer.io>
2020-11-27 19:14:16 +0300

    feat: add resources page and data, update version

---
+++ src/data/resources.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const resources = [
+  // Please add in alphabetical order of title.
+  {
+    title: 'Qiwi: Control the Data with Database Lab to Accelerate Development',
+    description: 'Learn how banks can overcome test data management pitfalls to eliminate production downtime related to data operations.',
+    preview: '/assets/thumbnails/case-study-qiwi.png',
+    link: '/download/database-lab-case-study-qiwi-rocketbank.pdf',
+    kind: 'Success story',
+    date: '2020-11-11 15:00:00',
+  },
+  {
+    title: 'CDEK: Speed up Product Development with Database Lab',
+    description: 'Learn how Database Lab speeding up development in a logistics company.',
+    preview: '/assets/thumbnails/case-study-cdek.png',
+    link: '/download/database-lab-case-study-cdek.pdf',
+    kind: 'Success story',
+    date: '2020-11-03 15:00:00',
+  },
+  {
+    title: 'Tutorial: Database Lab Engine',
+    description: '',
+    preview: '/assets/thumbnails/katakoda-tutorial-dle.png',
+    link: 'https://www.katacoda.com/postgres-ai/scenarios/database-lab-tutorial',
+    kind: 'Tutorial',
+    date: "2020-11-01 15:00:00",
+  },
+  {
+    title: 'Learn about best practices in preparing PostgreSQL databases for peak seasons in e-commerce companies.',
+    description: 'Learn about best practices in preparing PostgreSQL databases for peak seasons in e-commerce companies.',
+    preview: '/assets/thumbnails/wp1.png',
+    link: '/get-whitepaper-how-to-prepare-postgreqsl-databases-for-black-friday-and-cyber-monday',
+    kind: 'White paper',
+    date: "2020-11-05 15:00:00",
+  },
+];
+
+export default resources;
--- src/pages/resources/index.js
+++ src/pages/resources/index.js
@@ -1,105 +1,71 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import React from 'react';
+
 import Layout from '@theme/Layout';
 
-function Hello() {
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import resources from '../../data/resources';
+
+const TITLE = 'Resources';
+
+function Showcase() {
   return (
-    <Layout title="Hello">
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '20vh',
-          fontSize: '20px',
-        }}>
-        <p>
-          TODO: Resources table here.
-        </p>
-      </div>
-      <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '20vh',
-        fontSize: '20px',
-      }}>
-        <ul>
-          <li><a target="_blank" href="/download/database-lab-case-study-cdek.pdf">Case study: CDEK</a></li>
-          <li><a target="_blank" href="/download/database-lab-case-study-qiwi-rocketbank.pdf">Case study: QIWI</a></li>
-          <li><a target="_blank" href="https://www.katacoda.com/postgres-ai/scenarios/database-lab-tutorial">Katacoda: Database Lab tutorial</a></li>
-          <li><a target="_blank" href="/get-whitepaper-how-to-prepare-postgreqsl-databases-for-black-friday-and-cyber-monday">How to Prepare PostgreSQL Databases for Black Friday</a></li>
-        </ul>
-      </div>
+    <Layout title={TITLE}>
+      <main className="container margin-vert--lg">
+        <div className="text--center margin-bottom--m">
+          <h1>{TITLE}</h1>
+        </div>
+        <div className="row">
+          {resources.map((resource) => (
+            <div key={resource.title} className="col col--4 margin-bottom--lg">
+                <div className={clsx('card', styles.showcaseresource)}>
+                  <div className="card__image">
+                    <a
+                      href={resource.link}
+                      target="_blank"
+                      rel="noreferrer noopener">
+                        <img src={resource.preview} alt={resource.title} />
+                    </a>
+                  </div>
+                  <div className="card__body">
+                    <div className="avatar">
+                      <div className="avatar__intro margin-left--none">
+                        <h4 className="avatar__name">{resource.title}</h4>
+                        <small className="avatar__subtitle">
+                          {resource.description}
+                        </small>
+                      </div>
+                    </div>
+                  </div>
+                  {(resource.link) && (
+                    <div className="card__footer">
+                      <div className="button-group button-group--block">
+                        {resource.link && (
+                          <a
+                            className="button button--small button--secondary button--block"
+                            href={resource.link}
+                            target="_blank"
+                            rel="noreferrer noopener">
+                            Read more
+                          </a>
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </Layout>
   );
 }
 
-export default Hello;
-
-/*
-Do not delete until moved to proper separate pages!
-
----
-linktitle: "CDEK: Speed up Product Development with Database Lab"
-title: "CDEK: Speed up Product Development with Database Lab"
-date: "2020-11-03 15:00:00"
-description: "Learn how Database Lab speeding up development in a logistics company."
-weight: 0
-image: /assets/images/thumbnails/case-study-cdek.png
-kind: Case study
-redirect: /download/database-lab-case-study-cdek.pdf
-tags:
-  - Postgres.ai
-  - Database Lab
-  - PostgreSQL
-  - case study
-  - CDEK
-  - logistics
----
-linktitle: "Qiwi: Control the Data with Database Lab to Accelerate Development"
-title: "Qiwi: Control the Data with Database Lab to Accelerate Development"
-date: "2020-11-11 15:00:00"
-description: "Learn how banks can overcome test data management pitfalls to eliminate production downtime related to data operations."
-weight: 0
-image: /assets/images/thumbnails/case-study-qiwi.png
-kind: Case study
-redirect: /download/database-lab-case-study-qiwi-rocketbank.pdf
-tags:
-  - Postgres.ai
-  - Database Lab
-  - PostgreSQL
-  - case study
-  - bank
-  - Qiwi
-  - RocketBank
-  - finance
----
-linktitle: "Tutorial: Database Lab Engine"
-title: "Tutorial: Database Lab Engine"
-date: "2020-11-01 15:00:00"
-weight: 0
-image: /assets/images/thumbnails/katakoda-tutorial-dle.png
-kind: Katacoda
-redirect: https://www.katacoda.com/postgres-ai/scenarios/database-lab-tutorial
-tags:
-  - Postgres.ai
-  - Database Lab
-  - PostgreSQL
-  - release
----
-linktitle: "Learn about best practices in preparing PostgreSQL databases for peak seasons in e-commerce companies."
-title: How to Prepare PostgreSQL Databases for Black Friday
-date: "2020-11-05 15:00:00"
-description: "Learn about best practices in preparing PostgreSQL databases for peak seasons in e-commerce companies."
-weight: 0
-image: /assets/images/thumbnails/wp1.png
-kind: White paper
-redirect: "/get-whitepaper-how-to-prepare-postgreqsl-databases-for-black-friday-and-cyber-monday"
-tags:
-  - Postgres.ai
-  - Database Lab
-  - PostgreSQL
-  - release
----
-*/
+export default Showcase;
