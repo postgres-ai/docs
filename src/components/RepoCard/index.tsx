2e82d761132e904ca133e1cfcf53295070e1e3a4
Anton Chashchin <anton.casin@gmail.com>
2022-02-11 17:53:22 +0300

    feat: added 'star us on github' button; update repo urls; refactored repos cards and styles;

---
+++ src/components/RepoCard/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import GitHubButton from 'react-github-btn'
+
+import { GatewayLink } from '@site/src/components/GatewayLink'
+
+import styles from './styles.module.css'
+
+type Props = {
+  title: string
+  logoUrl: string
+  description: string
+  repoName: string
+  repoUrl: string
+  githubButtonText?: string
+}
+
+export const RepoCard = (props: Props) => {
+  return (
+    <div className={styles.root}>
+      <div>
+        <div className={styles.header}>
+          <h4 className={styles.title}>{props.title}</h4>
+          <img src={props.logoUrl} alt={props.title} className={styles.logo} />
+        </div>
+        <p className={styles.description}>{props.description}</p>
+      </div>
+
+      <div>
+        <GatewayLink className={styles.link} href={props.repoUrl}>
+          {props.repoName}
+        </GatewayLink>
+        {props.githubButtonText && (
+          <div className={styles.githubStarButtonWrapper}>
+            <GitHubButton
+              href={props.repoUrl}
+              // data-color-scheme="no-preference: light; light: light; dark: dark;"
+              data-color-scheme="light"
+              data-size="large"
+              data-show-count="true"
+              aria-label="Star ntkme/github-buttons on GitHub"
+            >
+              Star us
+            </GitHubButton>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
