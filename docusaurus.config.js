141447e0e750cb2e68e824e38d3850f9601c269a
Nikolay Samokhvalov <nik@postgres.ai>
2023-08-18 23:47:52 +0000

    post dblab 3.4 hotfixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ module.exports = {
     announcementBar: {
       id: 'advisory_group', // Any value that will identify this message to save the hidden status.
       content:
-        "<a href='/blog/20230819-dblab-engine-3-4'>Available now: DBLab 3.4 – install from Postgres.ai Console</a>",
+        "<a href='/blog/20230819-dblab-engine-3-4-released'>Available now: DBLab 3.4 – install from Postgres.ai Console</a>",
       backgroundColor: '#D7EEF2',
       textColor: '#013A44',
       isCloseable: true,
