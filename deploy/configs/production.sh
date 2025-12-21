1c4e1c926d5752d1836b34c90a39e252fcd9df07
Nik Samokhvalov <nik@Niks-MacBook-Pro.local>
2025-12-21 14:33:03 -0800

    Add Umami analytics integration

--- deploy/configs/production.sh
+++ deploy/configs/production.sh
@@ -4,3 +4,6 @@ export BASE_URL="/"
 export SIGN_IN_URL="https://console.postgres.ai/signin"
 export BOT_WS_URL="wss://postgres.ai/ai-bot-ws/"
 export API_URL_PREFIX="https://postgres.ai/api/general"
+# Umami analytics
+export UMAMI_WEBSITE_ID="3e8e7852-a623-4205-aa28-110f8c98e62e"
+export UMAMI_SCRIPT_URL="https://analytics.postgres.ai/script.js"
--- deploy/configs/staging.sh
+++ deploy/configs/staging.sh
@@ -4,3 +4,6 @@ export BASE_URL="/"
 export SIGN_IN_URL="https://console-v2.postgres.ai/signin"
 export BOT_WS_URL="wss://v2.postgres.ai/ai-bot-ws/"
 export API_URL_PREFIX="https://v2.postgres.ai/api/general"
+# Umami analytics
+export UMAMI_WEBSITE_ID="8a51b37c-420b-4777-8bab-63491be1a4ac"
+export UMAMI_SCRIPT_URL="https://analytics.postgres.ai/script.js"
