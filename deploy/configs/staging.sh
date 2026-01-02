97bc522abca6d83fd401162201e0090e89b89059
Tanya301 <tanya@samokhvalov.com>
2026-01-02 12:09:57 -0800

    Use analytics-v2 for staging Umami

--- deploy/configs/staging.sh
+++ deploy/configs/staging.sh
@@ -6,4 +6,4 @@ export BOT_WS_URL="wss://v2.postgres.ai/ai-bot-ws/"
 export API_URL_PREFIX="https://v2.postgres.ai/api/general"
 # Umami analytics
 export UMAMI_WEBSITE_ID="8a51b37c-420b-4777-8bab-63491be1a4ac"
-export UMAMI_SCRIPT_URL="https://analytics.postgres.ai/script.js"
+export UMAMI_SCRIPT_URL="https://analytics-v2.postgres.ai/script.js"
